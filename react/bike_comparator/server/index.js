538227de7aee059863e398316d77608c23224e3b
pem <pierre-etienne.moreau@loria.fr>
2018-07-18 19:03:46 +0200

    indent

--- react/bike_comparator/server/index.js
+++ react/bike_comparator/server/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
@@ -12,31 +11,33 @@ var db;
 var frames;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGO_URL, function (err, database) {
+mongodb.MongoClient.connect(
+  process.env.MONGO_URL,
+  function(err, database) {
     if (err) {
-        console.log(err);
-        process.exit(1);
+      console.log(err);
+      process.exit(1);
     }
 
     // Save database object from the callback for reuse.
-    db = database.db('frames'); //database;
-    frames = db.collection('frames');
+    db = database.db("frames"); //database;
+    frames = db.collection("frames");
 
     console.log("Database connection ready");
 
-
     // Initialize the app.
-    var server = app.listen(process.env.PORT || 8080, function () {
-        var port = server.address().port;
-        console.log("App now running on port", port);
+    var server = app.listen(process.env.PORT || 8080, function() {
+      var port = server.address().port;
+      console.log("App now running on port", port);
     });
-});
+  }
+);
 
 // CONTACTS API ROUTES BELOW
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({ error: message });
 }
 
 /**
@@ -46,96 +47,119 @@ function handleError(res, reason, message, code) {
  * example: findField({ brand:'Time' }, 'model')
  * returns a list of 'model' (each model appears only once) for the brand 'Time'
  */
-const findField = function(keys,field,res) {
-    frames.find(keys).map(function(entry) {return entry[field];}).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get " + field + ".");
-        } else {
-            res.status(200).json(docs);
-        }
-        ;
+const findField = function(keys, field, res) {
+  frames
+    .find(keys)
+    .map(function(entry) {
+      return entry[field];
+    })
+    .toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get " + field + ".");
+      } else {
+        res.status(200).json(docs);
+      }
     });
 };
 
-
-
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 /*  "/brands"
  *    GET: finds all contacts
  */
 app.get("/all", function(req, res) {
+  // if (req.method === 'OPTIONS') {
+  //     res.setHeader('Access-Control-Allow-Origin', '*');
+  //     res.setHeader('Access-Control-Request-Method', '*');
+  //     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  //     res.setHeader('Access-Control-Allow-Headers', '*');
+  //     res.writeHead(200);
+  //     res.end();
+  //     return;
+  // }
+  //
+  // res.setHeader('Content-Type', 'application/json; charset=UTF-8');
 
-    // if (req.method === 'OPTIONS') {
-    //     res.setHeader('Access-Control-Allow-Origin', '*');
-    //     res.setHeader('Access-Control-Request-Method', '*');
-    //     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    //     res.setHeader('Access-Control-Allow-Headers', '*');
-    //     res.writeHead(200);
-    //     res.end();
-    //     return;
-    // }
-    //
-    // res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+  // var n = frames.count();
+  // n.then(function(value) {
+  //     console.log(value);
+  // });
 
-    // var n = frames.count();
-    // n.then(function(value) {
-    //     console.log(value);
-    // });
+  // var result = [];
+  // const all = frames.find({}).forEach( function(elem) {
+  //     const {_id, brand, model, size, year} = elem;
+  //     console.log(_id, brand, model, size, year);
+  //     result.push({_id:_id, brand:brand, model:model, size:size, year:year});
+  // });
 
-    // var result = [];
-    // const all = frames.find({}).forEach( function(elem) {
-    //     const {_id, brand, model, size, year} = elem;
-    //     console.log(_id, brand, model, size, year);
-    //     result.push({_id:_id, brand:brand, model:model, size:size, year:year});
-    // });
+  // .map( function(elem) {
+  //     const {_id, brand, model, size, year} = elem;
+  //     return  {_id:_id, brand:brand, model:model, size:size, year:year};
+  // })
 
-    // .map( function(elem) {
-    //     const {_id, brand, model, size, year} = elem;
-    //     return  {_id:_id, brand:brand, model:model, size:size, year:year};
-    // })
+  frames
+    .find({})
+    .map(({ _id, brand, model, size, year }) => ({
+      _id: _id,
+      brand: brand,
+      model: model,
+      size: size,
+      year: year
+    }))
+    .toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get frames.");
+      } else {
+        // console.log(docs);
+        res.status(200).json(docs);
+      }
+    });
 
-    frames
-        .find({})
-        .map( ({_id, brand, model, size, year}) => ({_id:_id, brand:brand, model:model, size:size, year:year}) )
-        .toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get frames.");
-        } else {
-            // console.log(docs);
-            res.status(200).json(docs);
-        }});
-
-    // res.send(
-    //     JSON.stringify(result));
-        // find({}).toArray(function(err, docs) {
-        // if (err) {
-        //     handleError(res, err.message, "Failed to get frames.");
-        // } else {
-        //     res.status(200).json(docs);
-        // }
-    // });
+  // res.send(
+  //     JSON.stringify(result));
+  // find({}).toArray(function(err, docs) {
+  // if (err) {
+  //     handleError(res, err.message, "Failed to get frames.");
+  // } else {
+  //     res.status(200).json(docs);
+  // }
+  // });
 });
 
 app.get("/brands", function(req, res) {
-    findField({}, 'brand', res);
+  findField({}, "brand", res);
 });
 
 // app.get("/models", function(req, res) {
 //     findField({brand:req.param('brand')}, 'model', res);
 // });
 app.get("/models/:brand", function(req, res) {
-    let brand = req.params.brand;
-    findField({brand:brand}, 'model', res);
+  let brand = req.params.brand;
+  findField({ brand: brand }, "model", res);
 });
 app.get("/sizes", function(req, res) {
-    findField({brand:req.param('brand'), model:req.param('model')}, 'size', res);
+  findField(
+    { brand: req.param("brand"), model: req.param("model") },
+    "size",
+    res
+  );
 });
 
 app.get("/years", function(req, res) {
-    findField({brand:req.param('brand'), model:req.param('model'), size:req.param('size')}, 'year', res);
+  findField(
+    {
+      brand: req.param("brand"),
+      model: req.param("model"),
+      size: req.param("size")
+    },
+    "year",
+    res
+  );
 });
--- react/bike_comparator/src/App.js
+++ react/bike_comparator/src/App.js
@@ -1,167 +1,192 @@
-import React, { Component } from 'react';
-import DownshiftBike from './DownshiftBike';
-import Bike from './Bike';
+import React, { Component } from "react";
+import DownshiftBike from "./DownshiftBike";
+import Bike from "./Bike";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            data: [],
-            isLoading: false,
-            error: null,
+    this.state = {
+      data: [],
+      isLoading: false,
+      error: null,
 
-            tree: {},
-            // mytree: {
-            //     'Time': {
-            //             'NXR': {'XS': {'2011':1, '2010':2},
-            //                     'S': {'2011':3}
-            //             },
-            //             'Skylon': {'XS': {2015:4},
-            //                        'S': {2015:5}
-            //             },
-            //         }
-            // },
+      tree: {},
+      // mytree: {
+      //     'Time': {
+      //             'NXR': {'XS': {'2011':1, '2010':2},
+      //                     'S': {'2011':3}
+      //             },
+      //             'Skylon': {'XS': {2015:4},
+      //                        'S': {2015:5}
+      //             },
+      //         }
+      // },
 
-            selectedBrand: null,
-            selectedModel: null,
-            selectedSize: null,
-            selectedYear: null,
-            selectedBike: null,
-        };
+      selectedBrand: null,
+      selectedModel: null,
+      selectedSize: null,
+      selectedYear: null,
+      selectedBike: null
+    };
 
-        this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  initTree = function() {
+    for (var elem of this.state.data) {
+      const { brand, model, size, year, _id } = elem;
+      if (!this.state.tree[brand]) {
+        this.setState((this.state.tree[brand] = {}));
+      }
+      if (!this.state.tree[brand][model]) {
+        this.state.tree[brand][model] = {};
+      }
+      if (!this.state.tree[brand][model][size]) {
+        this.state.tree[brand][model][size] = {};
+      }
+      if (!this.state.tree[brand][model][size][year]) {
+        this.state.tree[brand][model][size][year] = _id;
+      }
+      // console.log(brand,model,size,year,_id);
     }
+    // console.log(this.state.tree['Time']);
+  };
 
-    initTree = function() {
-        for(var elem  of this.state.data) {
-            const {brand,model,size,year,_id} = elem;
-            if(!this.state.tree[brand]) {
-                this.setState(this.state.tree[brand] = {});
-            }
-            if(!this.state.tree[brand][model]) {
-                this.state.tree[brand][model] = {};
-            }
-            if(!this.state.tree[brand][model][size]) {
-                this.state.tree[brand][model][size] = {};
-            }
-            if(!this.state.tree[brand][model][size][year]) {
-                this.state.tree[brand][model][size][year] = _id;
-            }
-            // console.log(brand,model,size,year,_id);
+  getId = function() {
+    const {
+      selectedBrand,
+      selectedModel,
+      selectedSize,
+      selectedYear
+    } = this.state;
+    return selectedBrand && selectedModel && selectedSize && selectedYear
+      ? Object.keys(
+          this.state.tree[selectedBrand][selectedModel][selectedSize][
+            selectedYear
+          ]
+        )
+      : [];
+  };
 
+  // initBrands = function() {
+  //     if(this.state.brands.length === 0) {
+  //         const brands = this.state.data.map((x) => x.brand);
+  //         this.setState({brands: _.uniq(brands)});
+  //     }
+  //     console.log('#brands:', this.state.brands.length);
+  // }
+
+  // initModels = function() {
+  //     if(this.state.brands.length === 0) {
+  //         const brands = this.state.data.map((x) => x.brand);
+  //         this.setState({brands: _.uniq(brands)});
+  //     }
+  //     console.log('#brands:', this.state.brands.length);
+  // }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // const targetUrl = 'http://localhost:8080/models/Time';
+    const targetUrl = "http://localhost:8080/all";
+
+    const headers = new Headers();
+    // headers.append('Access-Control-Allow-Origin', '*');
+    const options = {
+      method: "GET",
+      headers: headers,
+      mode: "cors",
+      cache: "default"
+    };
+
+    fetch(targetUrl, options)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong ...");
         }
-        // console.log(this.state.tree['Time']);
-    }
+      })
+      .then(data => this.setState({ data: data, isLoading: false }))
+      .then(() => {
+        // console.log('Init data');
+        this.initTree();
+      })
+      .catch(error => console.log("Request failed", error));
+  }
 
-    getId = function() {
-        const { selectedBrand, selectedModel, selectedSize, selectedYear } = this.state;
-        return selectedBrand && selectedModel && selectedSize && selectedYear?
-            Object.keys(this.state.tree[selectedBrand][selectedModel][selectedSize][selectedYear]) : [];
-    }
+  render() {
+    const {
+      tree,
+      selectedBrand,
+      selectedModel,
+      selectedSize,
+      selectedYear,
+      selectedBike
+    } = this.state;
+    return (
+      <div className="container">
+        <header>
+          <h1>
+            Welcome to Bike Comparator!<br />
+            (React version)
+          </h1>
+        </header>
+        <h3>This web site will help you to find a bike similar to yours!</h3>
 
+        <h3>Select your bike</h3>
+        <DownshiftBike
+          field="brand"
+          getItems={() => Object.keys(tree)}
+          setSelected={item => this.setState({ selectedBrand: item })}
+        />
+        <DownshiftBike
+          field={"model"}
+          getItems={() =>
+            selectedBrand ? Object.keys(tree[selectedBrand]) : []
+          }
+          setSelected={item => this.setState({ selectedModel: item })}
+        />
+        <DownshiftBike
+          field={"size"}
+          getItems={() =>
+            selectedBrand && selectedModel
+              ? Object.keys(tree[selectedBrand][selectedModel])
+              : []
+          }
+          setSelected={item => this.setState({ selectedSize: item })}
+        />
+        <DownshiftBike
+          field={"year"}
+          getItems={() =>
+            selectedBrand && selectedModel && selectedSize
+              ? Object.keys(tree[selectedBrand][selectedModel][selectedSize])
+              : []
+          }
+          setSelected={item => this.setState({ selectedYear: item })}
+        />
 
-    // initBrands = function() {
-    //     if(this.state.brands.length === 0) {
-    //         const brands = this.state.data.map((x) => x.brand);
-    //         this.setState({brands: _.uniq(brands)});
-    //     }
-    //     console.log('#brands:', this.state.brands.length);
-    // }
+        <br />
+        <button
+          disabled={
+            !(selectedBrand && selectedModel && selectedSize && selectedYear)
+          }
+          onClick={() => true}
+        >
+          Run Comparator
+        </button>
 
-    // initModels = function() {
-    //     if(this.state.brands.length === 0) {
-    //         const brands = this.state.data.map((x) => x.brand);
-    //         this.setState({brands: _.uniq(brands)});
-    //     }
-    //     console.log('#brands:', this.state.brands.length);
-    // }
-
-
-    componentDidMount() {
-        this.setState({ isLoading: true });
-
-        // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        // const targetUrl = 'http://localhost:8080/models/Time';
-        const targetUrl = 'http://localhost:8080/all';
-
-        const headers = new Headers();
-        // headers.append('Access-Control-Allow-Origin', '*');
-        const options = {
-            method: 'GET',
-            headers: headers,
-            mode: 'cors',
-            cache: 'default',
-        };
-
-        fetch(targetUrl, options)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-            })
-            .then(data => this.setState({ data:data, isLoading: false }))
-            .then(() => {
-                // console.log('Init data');
-                this.initTree();
-            })
-            .catch(error => console.log('Request failed', error));
-    }
-
-    render() {
-        const { tree, selectedBrand, selectedModel, selectedSize, selectedYear, selectedBike } = this.state;
-        return (
-            <div className="container">
-                <header>
-                    <h1>
-                        Welcome to Bike Comparator!<br />
-                        (React version)
-                    </h1>
-                </header>
-                <h3>
-                    This web site will help you to find a bike similar to yours!
-                </h3>
-
-                <h3>Select your bike</h3>
-                <DownshiftBike
-                    field="brand"
-                    getItems={() => Object.keys(tree)}
-                    setSelected={(item) => this.setState({selectedBrand:item})}
-                />
-                <DownshiftBike
-                    field={'model'}
-                    getItems={() => selectedBrand ? Object.keys(tree[selectedBrand]) : []}
-                    setSelected={(item) => this.setState({selectedModel:item})}
-                />
-                <DownshiftBike
-                    field={'size'}
-                    getItems={() => selectedBrand && selectedModel ?
-                        Object.keys(tree[selectedBrand][selectedModel]) : []}
-                    setSelected={(item) => this.setState({selectedSize:item})}
-                />
-                <DownshiftBike
-                    field={'year'}
-                    getItems={() => selectedBrand && selectedModel && selectedSize ?
-                        Object.keys(tree[selectedBrand][selectedModel][selectedSize]) : []}
-                    setSelected={(item) => this.setState({selectedYear:item})}
-                />
-
-                <br />
-                <button disabled={!(selectedBrand && selectedModel && selectedSize && selectedYear)} onClick={() => true}>
-                    Run Comparator
-                </button>
-
-                <h3>Selected bike</h3>
-                <ul>
-                    {selectedBike ? (
-                        <Bike key={selectedBike._id} bike={selectedBike} />
-                    ) : (
-                        <li>No selected bike</li>
-                    )}
-                </ul>
-                {/*
+        <h3>Selected bike</h3>
+        <ul>
+          {selectedBike ? (
+            <Bike key={selectedBike._id} bike={selectedBike} />
+          ) : (
+            <li>No selected bike</li>
+          )}
+        </ul>
+        {/*
               <h3>Top bikes</h3>
               <ul>
                   {this.props.top_bikes.map((pair) => (
@@ -169,9 +194,9 @@ class App extends Component {
                   ))}
               </ul>
                 */}
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
